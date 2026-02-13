a842983ecba798211881ff309fa321f2489d2eb9
Tomasz Naumowicz <tnaumowicz@microsoft.com>
2026-02-13 15:21:07 +0100

    Step 3: Add button and message display to Basic View; update styles

--- src/webviews/demo/basicView/BasicView.tsx
+++ src/webviews/demo/basicView/BasicView.tsx
@@ -3,14 +3,24 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Title1 } from '@fluentui/react-components';
+import { Body1, Button, Title1 } from '@fluentui/react-components';
 import * as l10n from '@vscode/l10n';
+import { useState } from 'react';
 import './basicView.scss';
 
 export const BasicView: React.FC = () => {
+    const [message, setMessage] = useState<string>('');
+
     return (
         <div className="basicView">
             <Title1>{l10n.t('Basic View')}</Title1>
+
+            <div className="basicView__actions">
+                <Button appearance="primary" onClick={() => setMessage(l10n.t('Button Pressed'))}>
+                    {l10n.t('Press Me')}
+                </Button>
+                {message && <Body1>{message}</Body1>}
+            </div>
         </div>
     );
 };
